a3472124960d94025653dfef340a95b717d3e8ed
Eric Li <eric.li.8736@gmail.com>
2025-11-17 13:27:44 -0600

    Add physics stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss(), wasm(), topLevelAwait()],
+    },
 });

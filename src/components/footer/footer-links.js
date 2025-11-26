baeeb798ffaa6345cb554e6eb27aaab1562053ec
Ju5t1nL3 <justinle2006@gmail.com>
2025-11-25 21:02:00 -0600

    Add footer

---
+++ src/components/footer/footer-links.js
@@ -0,0 +1,91 @@
+export const resources = [
+		//   {
+		//       name: 'Hacker Guide',
+		//       link: 'https://drive.google.com/file/d/1gUGimWC_s4eOu4OGOS6Hj2IqaOg9oWPt/view',
+		// // TODO: update link
+		//   },
+    {
+        name: 'HelpR',
+        link: 'https://helpr.tamuhack.org/',
+    },
+    {
+        name: 'Code of Conduct',
+        link: 'https://static.mlh.io/docs/mlh-code-of-conduct.pdf',
+    },
+		//   {
+		//       name: 'Misconduct Reporting',
+		//       link: 'https://tamuhack.org/misconduct',
+		// // TODO: update misconduct form
+		//   },
+		//   {
+		//       name: 'Devpost',
+		//       link: 'https://tamuhack-2025.devpost.com/',
+		// //TODO: create devpost
+		//   },
+];
+
+export const socials = [
+    {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/tamuhack',
+    },
+    {
+        name: 'TikTok',
+        link: 'https://www.tiktok.com/@tamuhack',
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/company/tamuhack',
+    },
+    {
+        name: 'Discord',
+        link: 'https://tamuhack.org/discord',
+    },
+];
+
+export const otherHackathons = [
+    {
+        name: 'TAMU Datathon',
+        link: 'https://tamudatathon.com/',
+    },
+    {
+        name: 'HackTX',
+        link: 'https://hacktx.com/',
+    },
+    {
+        name: 'HackUTD',
+        link: 'https://hackutd.co/',
+    },
+    {
+        name: 'HackUNT',
+        link: 'https://www.unthackathon.com/',
+    },
+    {
+        name: 'RowdyHacks',
+        link: 'https://rowdyhacks.org/',
+    },
+    {
+        name: 'HackRice',
+        link: 'https://www.hackrice.com/',
+    },
+];
+
+export const workshops = [
+	//TODO: update these links when we redo workshop slides
+    {
+        name: 'Intro to Hackathons',
+        link: 'https://docs.google.com/presentation/d/1r142lnKlA043jyYcFnjSFebxcm52usJ9MIVpLyUWQI4/edit?usp=sharing',
+    },
+    {
+        name: 'Intro to Web Dev',
+        link: 'https://docs.google.com/presentation/d/16moIOAhsbH5qlyeWv0xfH73XT_hk0TJ-b6xct3njT5U/edit?usp=sharing',
+    },
+    {
+        name: 'Intro to Git',
+        link: 'https://docs.google.com/presentation/d/17tD4eOPL54v6YPEZE57gkOrtEo9LA_r4U0bAOBRGRSo/edit?usp=sharing',
+    },
+    {
+        name: 'Intro to Hardware',
+        link: 'https://docs.google.com/presentation/d/1PGyzuwHUsFabeBiMDdyGySel6rtKnuKERMko81i1Lb0/edit?usp=sharing',
+    },
+];

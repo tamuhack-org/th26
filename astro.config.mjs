482f57316a3138ebf712bed0dab6efe0a117be3c
Eric Li <eric.li.8736@gmail.com>
2025-12-01 13:00:31 -0600

    Use new font api
    it's experimental but easier than trying to do fontsource's preload docs
    tbh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Stack Sans Text",
+        cssVariable: "--font-stack-sans",
+        weights: [400, 500, 600, 700],
+        styles: ["normal"]
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "BBH Sans Hegarty",
+        cssVariable: "--font-bbh-sans",
+        display: "block"
+      }
+    ]
+  },
   vite: {
     plugins: [tailwindcss()]
   }

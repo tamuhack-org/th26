ab1016c0e6cd1bdea582dcaf87459f75ef4a3b7a
Ju5t1nL3 <justinle2006@gmail.com>
2025-12-30 18:35:46 -0600

    Add real non-sale prices for prizes and pad ending in receipt

--- src/components/prizes/prizes.ts
+++ src/components/prizes/prizes.ts
@@ -17,34 +17,34 @@ export const prizeslist: Record<string, Prize> = {
     'first-place-software': {
         placement: 'first',
         track: 'software',
-        prizes: [['Macbook Air M2', 9.99]],
+        prizes: [['Macbook Air M2', 799.0]],
         description: 'First place software track.',
         type: Category.SOFTWARE,
     },
     'second-place-software': {
         placement: 'second',
         track: 'software',
-        prizes: [['iPad A16', 9.99]],
+        prizes: [['iPad A16', 350.0]],
         description: 'Second place software track.',
         type: Category.SOFTWARE,
     },
     'third-place-software': {
         placement: 'third',
         track: 'software',
-        prizes: [['SteelSeries Gaming Keyboard', 9.99]],
+        prizes: [['SteelSeries Gaming Keyboard', 219.99]],
         description: 'Third place software track.',
         type: Category.SOFTWARE,
     },
     'best-design': {
         track: 'best design',
-        prizes: [['Polaroid Now 3rd Gen', 9.99]],
+        prizes: [['Polaroid Now 3rd Gen', 129.99]],
         description:
             'In the world of hacking and engineering, a product must not only work well, but also provide the best user experience possible. Best hack that demonstrates clear design and usability intentions.',
         type: Category.SOFTWARE,
     },
     'best-beginner-software': {
         track: 'best beginner software hack',
-        prizes: [['40" Smart TV', 9.99]],
+        prizes: [['40" Smart TV', 149.99]],
         description:
             'Best software hack created by first-time hackers. To qualify, teams must be at least 50% composed of first-timers at a hackathon.',
         type: Category.SOFTWARE,
@@ -52,39 +52,42 @@ export const prizeslist: Record<string, Prize> = {
     'first-place-hardware': {
         placement: 'first',
         track: 'hardware',
-        prizes: [['Gotrax Electric Scooter', 9.99]],
+        prizes: [['Gotrax Electric Scooter', 299.0]],
         description: 'First place hardware track.',
         type: Category.HARDWARE,
     },
     'second-place-hardware': {
         placement: 'second',
         track: 'hardware',
-        prizes: [['Neptune 3 Pro 3D Printer', 9.99]],
+        prizes: [['Neptune 3 Pro 3D Printer', 213.0]],
         description: 'Second place hardware track.',
         type: Category.HARDWARE,
     },
     'third-place-hardware': {
         placement: 'third',
         track: 'hardware',
-        prizes: [['Sony WH-CH720N', 9.99]],
+        prizes: [['Sony WH-CH720N', 179.99]],
         description: 'Third place hardware track.',
         type: Category.HARDWARE,
     },
     iot: {
         track: 'IoT Challenge',
-        prizes: [['Soldering Kit and Digital Multimeter', 9.99]],
+        prizes: [
+            ['Soldering Kit', 39.99],
+            ['Compact digital multimeter', 42.99],
+        ],
         description: 'IoT challenge track.',
         type: Category.HARDWARE,
     },
     'medical-device': {
         track: 'medical device challenge',
-        prizes: [['Marshall Speaker', 9.99]],
+        prizes: [['Marshall Speaker', 179.99]],
         description: 'Medical device challenge track.',
         type: Category.HARDWARE,
     },
     'best-beginner-hardware': {
         track: 'best beginner hardware hack',
-        prizes: [['Arduino Starter Kit R4', 9.99]],
+        prizes: [['Arduino Starter Kit R4', 95.0]],
         description:
             'Best hardware hack created by first-time hackers. To qualify, teams must be at least 50% composed of first-timers at a hackathon.',
         type: Category.HARDWARE,
